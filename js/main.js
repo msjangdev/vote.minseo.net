baeb6f5a003041bb1e2f9e6eed11f73caa633be7
Minseo Jang <gs24091@gs.hs.kr>
2024-11-27 16:22:23 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -85,12 +85,13 @@
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
+	    loop:false,
+	    autoplay: false,
 	    margin:0,
 	    animateOut: 'fadeOut',
 	    animateIn: 'fadeIn',
 	    nav:false,
+		dots:true,
 	    autoplayHoverPause: false,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
